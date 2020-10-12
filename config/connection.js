5ed14f381342b6785273a67240444203598ad158
nosam1998 <nosam1998@gmail.com>
2020-10-11 22:43:28 -0700

    Added test and production mode

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "toor",
-    database: "burger_db"
-});
+if (process.env.TEST_MODE == true || process.env.TEST_MODE == null) {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "toor",
+        database: "burger_db"
+    });
+} else {
+    connnection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+
 
 // Make connection.
 connection.connect(function (err) {
